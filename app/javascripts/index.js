f82f7fb813d6b1a0ae8933d6aeed303c097e128c
dzucconi <mail@damonzucconi.com>
2016-12-04 12:56:41 +0100

    Link to topics

--- app/javascripts/index.js
+++ app/javascripts/index.js
@@ -7,17 +7,20 @@ const DOM = {
   app: document.getElementById('app'),
 };
 
-const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const alphabet = '#abcdefghijklmnopqrstuvwxyz'.split('');
 
 export default () => {
   const { amount } = parameters({
-    amount: 0.1,
+    amount: 0.01,
   });
 
   const render = factor => alphabet.map((x, i) => `
     <div class='row'>
       ${alphabet.map((y, n) => `
-        <div class='cell' style='transform: rotate(${n * i * factor}deg)'>${x + y}</div>
+        <a class='cell cell--centered' style='transform: rotate(${n * i * factor}deg)'
+          href='https://www.quora.com/sitemap/alphabetical_topics/${(x === '#' ? '0' : x) + (y === '#' ? '0' : y)}'>
+          ${x + y}
+        </a>
       `).join('')}
     </div>
   `).join('');
